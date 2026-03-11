4a360635ff02e9db52157acfbbbebaa809417f7e
Huu Thuan <huuthuantran434@gmail.com>
2026-03-11 10:27:40 +0700

    fix server

--- server.js
+++ server.js
@@ -7,31 +7,39 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 
-mongoose.connect(process.env.MONGO_URI)
+mongoose.connect(process.env.MONGO_URI,{
+  useNewUrlParser:true,
+  useUnifiedTopology:true
+})
+.then(()=>console.log("MongoDB connected"))
+.catch(err=>console.log(err))
+
 const BookingSchema = new mongoose.Schema({
-name:String,
-doctor:String,
-date:String,
-time:String
+  name:String,
+  doctor:String,
+  date:String,
+  time:String
 })
 
 const Booking = mongoose.model("Booking",BookingSchema)
-app.get("/", (req, res) => {
-  res.send("Doctor Booking API Running 🚀");
-});
+
+app.get("/",(req,res)=>{
+  res.send("Doctor Booking API Running 🚀")
+})
+
 app.post("/booking",async(req,res)=>{
-const booking = new Booking(req.body)
-await booking.save()
-res.send("success")
+  const booking = new Booking(req.body)
+  await booking.save()
+  res.send("success")
 })
 
 app.get("/booking",async(req,res)=>{
-const data = await Booking.find()
-res.json(data)
+  const data = await Booking.find()
+  res.json(data)
 })
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000
 
-app.listen(PORT, () => {
-  console.log("Server running on port " + PORT);
-});
+app.listen(PORT,()=>{
+  console.log("Server running on port " + PORT)
+})
