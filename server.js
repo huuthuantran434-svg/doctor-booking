431ed2ee590d1e3148619c7610ba030051ae7607
Huu Thuan <huuthuantran434@gmail.com>
2026-03-11 09:29:58 +0700

    fix render port

--- server.js
+++ server.js
@@ -29,6 +29,8 @@ const data = await Booking.find()
 res.json(data)
 })
 
-app.listen(5000,()=>{
-console.log("Server running on port 5000")
-})
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log("Server running on port " + PORT);
+});
